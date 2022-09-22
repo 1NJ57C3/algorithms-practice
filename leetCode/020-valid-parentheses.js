fc325859bcb090721907390c1eeefdc119bf11b2
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2022-09-22 02:15:20 -0700

    Solution: 20. Valid Parentheses

---
+++ leetCode/020-valid-parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isValid = function(s) {
+    const complements = {
+        '(' : ')',
+        '[' : ']',
+        '{' : '}'
+    };
+    
+    const arr = [];
+    
+    for(let i = 0; i < s.length; i++) {
+        if ((/[([{]/).test(s[i])) {
+            arr.push(s[i])
+        }
+        else if (s[i] === complements[arr[arr.length - 1]]) {
+            arr.pop()
+        }
+        else return false;
+    }
+    
+    if (arr.length === 0) return true
+    else return false;
+};
